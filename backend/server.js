aa157ef58e80638f806c5c838f1a67c0fb47c23d
Kenneth <ngken0995@gmail.com>
2021-08-29 21:12:41 -0400

    testing sendGrid

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,28 @@ app.use('/api', auth);
 app.use('/news', newsRouter);
 
 
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'kennethng2017@gmail.com',
+  from: 'ngken0995@gmail.com', // Use the email address or domain you verified above
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+//ES6
+sgMail
+  .send(msg)
+  .then(() => {}, error => {
+    console.error(error);
+
+    if (error.response) {
+      console.error(error.response.body)
+    }
+  });
+
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
