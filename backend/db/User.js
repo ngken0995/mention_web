00331df9c58f1613f17451a1fdb2306c9b37ccb0
ngken0995 <59739226+ngken0995@users.noreply.github.com>
2021-09-03 17:55:26 -0400

    edit

--- backend/db/User.js
+++ backend/db/User.js
@@ -26,25 +26,15 @@ userSchema.pre('save', function(next) {
   // If not registration
   if ( !user.isModified('password') ) return next()
 
-  bcrypt.hash(user.password, 10, (err, hash) => {
-    if (err) {
-      return next(err)
-    }
-    user.password = hash
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) throw err;
+      user.password = hash;
     next()
+    })
   })
 })
 
-// Password verification
-userSchema.methods.login = function(password) {
-  var user = this
-  return new Promise((resolve, reject) => {
-    bcrypt.compare(password, user.password, (err, result) => {
-      if ( err ) { reject(err) }
-      resolve()
-    })
-  })
-}
 
 // Export Mongoose "User" model
 module.exports = mongoose.model('User', userSchema)
