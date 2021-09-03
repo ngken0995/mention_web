12d9e9473a4dcdfced7645d6c1d959b78b6b2d97
ngken0995 <59739226+ngken0995@users.noreply.github.com>
2021-09-03 18:58:36 -0400

    edit signup.js

--- frontend/mention-front/src/components/Signup.js
+++ frontend/mention-front/src/components/Signup.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
-import isAuthenticated from '../lib/isAuthenticated'
+import React, { Component, useState, useEffect } from 'react'; 
+import { Redirect, useHistory } from 'react-router-dom';
+import isAuthenticated from '../lib/isAuthenticated';
 
-export default class Signup extends Component {
+function Signup() {
+  const [loggedin, setLoggedin] = useState(isAuthenticated());
+  const history = useHistory();
+  useEffect(() => {
+    if(loggedin){
+    history.push({
+      pathname: '/login'
+  });}
+  },[loggedin])
 
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      loggedin: isAuthenticated()
-    }
-  }
-
-  submit(e) {
+  const submit = (e) => {
     e.preventDefault()
     e.stopPropagation()
 
@@ -32,48 +32,37 @@ export default class Signup extends Component {
     // You must need to valide data but I skip in here
 
     // Send request to the server
-    fetch('http://localhost:5000/api/signup', {
+    fetch('/api/signup', {
       method: 'POST',
       body: params
     }).then( (res) => {
       return res.json()
     }).then(data => {
       localStorage.setItem('token', data.token)
-      this.setState({loggedin: true})
+      setLoggedin(true)
     }).catch( (err) => {
       console.error(err)
     })
   }
 
-  render() {
-    if ( this.state.loggedin ) {
-      return (
-        <Redirect
-          to={{
-            pathname: '/',
-            state: { from: this.props.location }
-          }}
-        />
-      )
-    } else {
-      return (
-        <div>
-          <h1>Signup</h1>
-          <form onSubmit={this.submit.bind(this)}>
-            <div>
-              <label>Username: </label>
-              <input type="text" name="username" pattern=".{2,16}" required />
-            </div>
-            <div>
-              <label>Password: </label>
-              <input type="password" name="password" pattern=".{6,20}" required />
-            </div>
-            <div>
-              <input type="submit" value="Sign up" />
-            </div>
-          </form>
-        </div>
-      )
-    }
-  }
+  return (
+    <div>
+    <h1>Signup</h1>
+    <form onSubmit={submit}>
+      <div>
+        <label>Username: </label>
+        <input type="text" name="username" pattern=".{2,16}" required />
+      </div>
+      <div>
+        <label>Password: </label>
+        <input type="password" name="password" pattern=".{6,20}" required />
+      </div>
+      <div>
+        <input type="submit" value="Sign up" />
+      </div>
+    </form>
+  </div>
+  )
 }
+
+export default Signup
